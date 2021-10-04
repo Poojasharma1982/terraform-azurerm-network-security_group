c3251e9d1c6231d8dc847705c142efd7aa3848d9
Poojasharma1982 <89822894+Poojasharma1982@users.noreply.github.com>
2021-10-04 17:10:35 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -2,7 +2,11 @@ variable "rg-nsg-name" {
   type = string
   # default = "01000000-0000-4000-8000-000030080200"
 }
-variable "rg-nsg-name" {
+variable "networksecuritygroup-name" {
+  type = string
+  # default = "01000000-0000-4000-8000-000030080200"
+}
+variable "networksecuritygroup-location" {
   type = string
   # default = "01000000-0000-4000-8000-000030080200"
 }
